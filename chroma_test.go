5541646df240eae102bc75d289c7cd0fb23976a2
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-11-18 20:23:39 +0100

    add basic test

---
+++ chroma_test.go
@@ -0,0 +1,17 @@
+package chroma_test
+
+import "testing"
+
+func TestParseInsert(t *testing.T) {
+	_ = []byte(`{
+		"op": "i",
+		"ns": "test.student",
+		"o":  {
+			"_id": "635b79e231d82a8ab1de863b",
+			"name": "John Doe",
+			"roll_no": 51,
+			"is_graduated": false,
+			"date_of_birth": "2000-01-30"
+		},
+	}`)
+}
