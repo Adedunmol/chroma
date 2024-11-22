1c98ee263447f6e969536462a88b1a6ade2f748d
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-11-22 08:42:03 +0100

    add parsing of update

--- update.go
+++ update.go
@@ -1,13 +1,113 @@
 package chroma
 
-type Update struct{}
+import (
+	"errors"
+)
+
+type Update struct {
+	Op       string
+	Database string
+	Table    string
+	Column   KeyValue
+	Query    KeyValue
+}
 
 func NewUpdate() Update {
 
 	return Update{}
 }
 
-func (u *Update) Parse(data []byte) (*Update, error) {
+func (u *Update) Parse(data map[string]interface{}) (*Update, error) {
+
+	ns := getNamespace(data)
+
+	match, err := extractNamespace(ns)
+
+	if err != nil {
+		return u, err
+	}
+
+	u.Database = match[1]
+	u.Table = match[2]
+
+	op, err := getOperation(data)
+	if err != nil {
+		return u, err
+	}
+
+	u.Op = op
+
+	u.Column = u.getColumns(data, u.Op)[0]
+
+	query, err := u.getQuery(data)
+
+	if err != nil {
+		return u, err
+	}
+	u.Query = query
 
 	return u, nil
+}
+
+func getOperation(data map[string]interface{}) (string, error) {
+
+	op, exists := data["o"].(map[string]interface{})["diff"]
+	if !exists {
+		return "", errors.New("no operation found")
+	}
+
+	var operation []string
+
+	for key, _ := range op.(map[string]interface{}) {
+		operation = append(operation, key)
+	}
+
+	if len(operation) == 0 {
+		return "", errors.New("no operation found")
+	}
+
+	switch operation[0] {
+	case "u":
+		return "u", nil
+	case "d":
+		return "d", nil
+	default:
+		return "", errors.New("unknown operation")
+	}
+
+}
+
+func (u *Update) getColumns(data map[string]interface{}, operation string) []KeyValue {
+
+	var result []KeyValue
+
+	object, ok := data["o"].(map[string]interface{})["diff"].(map[string]interface{})[operation]
+
+	if !ok {
+		return result
+	}
+
+	for key, value := range object.(map[string]interface{}) {
+		data := KeyValue{Key: key, Value: value}
+		result = append(result, data)
+	}
+
+	return result
+}
+
+func (u *Update) getQuery(data map[string]interface{}) (KeyValue, error) {
+	query, exists := data["o2"].(map[string]interface{})
+	if !exists {
+		return KeyValue{}, errors.New("no query found")
+	}
+
+	var result []KeyValue
+
+	for key, value := range query {
+		result = append(result, KeyValue{Key: key, Value: value})
+	}
+	if len(result) == 0 {
+		return KeyValue{}, errors.New("no query found")
+	}
+	return result[0], nil
 }
--- update_test.go
+++ update_test.go
@@ -1,45 +1,48 @@
 package chroma_test
 
 import (
+	"github.com/Adedunmol/chroma"
+	"reflect"
 	"testing"
 )
 
 func TestUpdate(t *testing.T) {
-	//oplog := []byte(`{
-	//	"op": "i",
-	//	"ns": "test.student",
-	//	"o":  {
-	//		"$v": 2,
-	//		"diff": {
-	//			"d": {
-	//				"roll_no": false
-	//			}
-	//		}
-	//	},
-	//	"o2": {
-	//		"_id": "635b79e231d82a8ab1de863b"
-	//	}
-	//}`)
-	//
-	//insert := chroma.NewInsert()
-	//got, err := insert.Parse(oplog)
-	//if err != nil {
-	//	t.Fatal(err)
-	//}
-	//
-	//want := chroma.Insert{
-	//	Database: "test",
-	//	Table:    "student",
-	//	Columns: []chroma.KeyValue{
-	//		{Key: "_id", Value: "635b79e231d82a8ab1de863b"},
-	//		{Key: "name", Value: "John Doe"},
-	//		{Key: "roll_no", Value: float64(51)},
-	//		{Key: "is_graduated", Value: false},
-	//		{Key: "date_of_birth", Value: "2000-01-30"},
-	//	},
-	//}
-	//
-	//if !reflect.DeepEqual(*got, want) {
-	//	t.Errorf("got %#v want %#v", *got, want)
-	//}
+	oplog := []byte(`{
+		"op": "u",
+		"ns": "test.student",
+		"o":  {
+			"$v": 2,
+			"diff": {
+				"u": {
+					"is_graduated": true
+				}
+			}
+		},
+		"o2": {
+			"_id": "635b79e231d82a8ab1de863b"
+		}
+	}`)
+
+	update := chroma.NewUpdate()
+	data, err := chroma.ParseJSONMap(oplog)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got, err := update.Parse(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := chroma.Update{
+		Op:       "u",
+		Database: "test",
+		Table:    "student",
+		Column:   chroma.KeyValue{Key: "is_graduated", Value: true},
+		Query:    chroma.KeyValue{Key: "_id", Value: "635b79e231d82a8ab1de863b"},
+	}
+
+	if !reflect.DeepEqual(*got, want) {
+		t.Errorf("got %#v want %#v", *got, want)
+	}
 }
